696746383fa57eefa3f4822613aa0fba23a2287c
maarlesh <subamaarleshbaskar@gmail.com>
2023-10-15 12:50:06 +0530

    Added shadow

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const camera = new THREE.PerspectiveCamera(90, aspectRatio, 0.1,1000);
 const renderer = new THREE.WebGL1Renderer();
 //setting size of the renderer
 renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.shadowMap.enabled = true; // Enable shadow mapping
 //appending the render to the dom of the body
 document.body.appendChild(renderer.domElement);
 
@@ -30,6 +31,9 @@ const cube = new THREE.Mesh(geometry,material);
 scene.add(cube);
 //By default, when we call scene.add(), the thing we add will be added to the coordinates (0,0,0). This would cause both the camera and the cube to be inside each other. To avoid this, we simply move the camera out a bit.
 camera.position.z = 5;
+//enabling shadow
+cube.castShadow = true;
+cube.receiveShadow = true;
 
 //5. Render the scene
 function animate(){
@@ -43,12 +47,13 @@ animate();
 
 //6.Creating a ground
 const groundGeometry = new THREE.PlaneGeometry(10,10);
-const groundMaterial = new THREE.MeshBasicMaterial( {color : 0xff80ff} );
+const groundMaterial = new THREE.MeshStandardMaterial( {color : 0xffccff } );
 const ground = new THREE.Mesh(groundGeometry,groundMaterial);
 //rotating the ground
 ground.rotation.x = -45;
 ground.position.y = -2;
 ground.receiveShadow = true;
+
 scene.add(ground);
 
 //7. Adding Light
@@ -56,6 +61,7 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(1, 1, 5);
 light.castShadow = true;
 scene.add(light);
+
 
 //8. Changing Scene Background color
 scene.background = new THREE.Color(0xffe6ff)
